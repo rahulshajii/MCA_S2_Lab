f7f3ded7ac03c1c24509ccf0689759886f50cbb8
rahulshajii <rahulshaji520@gmail.com>
2026-02-23 11:53:05 +0530

    java lab cycle

---
+++ Java/sample.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sample{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the 2 number:");
+        int num=sc.nextInt();
+        int num1=sc.nextInt();
+        System.out.println("the sum of the number is "+(num+num1));
+        
+    }
+}
